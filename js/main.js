207dc789fa83d643806e5c3a5fda7de73dca7c20
Noelia-F <noelia.fronton.g@gmail.com>
2017-12-06 23:30:59 +0100

    add two new functions to js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'use strict'
+
+var randomNumber = 5;
+var selectNumber = document.querySelector('.number');
+var button = document.querySelector('.try-btn');
+var textAlert = document.querySelector('.guess-text');
+var newTry = document.querySelector('.tries');
+function sendNumber() {
+		if (selectNumber.value === randomNumber) {
+			textAlert.innerHTML = "Acertado";
+		} else if (number < randomNumber) {
+			textAlert.innerHTML = "Número demasiado bajo";
+		} else {
+			textAlert.innerHTML = "Número demasiado alto";
+		}
+}
+
+function addTries(){
+	newTry.innerHTML = newTry++;
+}
+
+button.addEventListener('click', sendNumber);
+button.addEventListener('click', addTries);
