d44a4e911f3cd3607c49870d01d264aad52e2994
Noelia-F <noelia.fronton.g@gmail.com>
2017-12-08 14:19:05 +0100

    insert number to compare done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 'use strict'
 
-var randomNumber = 5;
+var randomNumber=5;
 var selectNumber = document.querySelector('.number');
 var button = document.querySelector('.try-btn');
 var textAlert = document.querySelector('.guess-text');
 var newTry = document.querySelector('.tries');
+selectNumber.value = parseInt(selectNumber.value);
+
 function sendNumber() {
-		if (selectNumber.value === randomNumber) {
+		if (selectNumber.value == randomNumber) {
 			textAlert.innerHTML = "Acertado";
-		} else if (number < randomNumber) {
+		} else if (selectNumber.value < randomNumber) {
 			textAlert.innerHTML = "Número demasiado bajo";
 		} else {
 			textAlert.innerHTML = "Número demasiado alto";
 		}
 }
 
-function addTries(){
-	newTry.innerHTML = newTry++;
-}
 
-button.addEventListener('click', sendNumber);
-button.addEventListener('click', addTries);
+button.addEventListener('click', sendNumber, generateRandomNumber);
